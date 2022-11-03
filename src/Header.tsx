9acaef6eeb12336a8aeba77a60f6d1d28f3e8d70
Joe Previte <jjprevite@gmail.com>
2022-11-02 21:47:58 -0700

    refactor: change name to Joe

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <div className="absolute top-0 block w-full mx-auto w-100 block w-100 bg-slate-200 py-2 border-slate-200 border-b-2">
-      <span className="font-bold text-black block">John Doe</span>
+      <span className="font-bold text-black block">Joe Doe</span>
     </div>
   );
 }
