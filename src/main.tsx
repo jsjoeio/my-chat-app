52a9c6f9b430f61cebd65a5f03dfe188b52cd41f
Joe Previte <jjprevite@gmail.com>
2022-11-10 15:03:36 -0700

    fix disable stuff for now

--- src/main.tsx
+++ src/main.tsx
@@ -5,23 +5,12 @@ import PullToRefresh from 'pulltorefreshjs';
 import App from './App';
 import './index.css';
 
-PullToRefresh.init({
-  mainElement: 'body',
-  onRefresh() {
-    window.location.reload();
-  },
-});
-
-const resizeOps = () => {
-  // document.documentElement.style.setProperty(
-  //   '--vh',
-  //   window.innerHeight * 0.01 + 'px'
-  // );
-  console.log("resize happened")
-};
-
-resizeOps();
-window.addEventListener('resize', resizeOps);
+// PullToRefresh.init({
+//   mainElement: 'body',
+//   onRefresh() {
+//     window.location.reload();
+//   },
+// });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
